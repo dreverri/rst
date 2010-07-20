304e369e05a5b4b516fec305afad3a64a6c6f2c8
Daniel Reverri <reverri@gmail.com>
2010-07-19 23:09:01 -0700

    Updated start_riak/1 to retrieve dependencies
    Dependencies are now retrieved from application:get_key/2 instead
    of using a hard coded list.

--- src/rst.erl
+++ src/rst.erl
@@ -4,6 +4,7 @@
 
 -export([start_node/4, 
          start_riak/1,
+         start_application/2,
          clone_repo/2,
          run_make/2
          ]).
@@ -15,6 +16,7 @@
 
 %% start node and add paths
 start_node(Host, Name, Arg, RiakDir) ->
+    %%error_logger:error_report(Arg),
     {ok, Node} = slave:start_link(Host, Name, Arg),
     AppEbins = filelib:wildcard(filename:join([RiakDir, "apps", "*", "ebin"])),
     DebEbins = filelib:wildcard(filename:join([RiakDir, "deps", "*", "ebin"])),
@@ -23,34 +25,27 @@ start_node(Host, Name, Arg, RiakDir) ->
 
 %% start riak on the given node
 start_riak(Node) ->
-    StartFun = fun() ->
-            lists:foldl(fun(App, Acc) ->
-                                case application:start(App) of
-                                    ok ->
-                                        [{ok, App}|Acc];
-                                    Error ->
-                                        error_logger:error_report([{application, App},
-                                                                   Error]),
-                                        [{error, App}|Acc]
-                                end
-                        end,
-                        [],
-                        [
-                         %% list of applications to start on remote node
-                         sasl,
-                         os_mon,
-                         crypto,
-                         mochiweb,
-                         webmachine,
-                         erlang_js,
-                         luke,
-                         bitcask,
-                         riak_core,
-                         riak_kv
-                        ])
-    end,
+    start_application(Node, riak_kv).
+
+start_application(Node, Application) when is_atom(Application) ->
+    Deps = collect_deps(Node, [Application], []),
+    start_application(Node, Deps ++ [Application]);
+                      
+start_application(Node, Apps) when is_list(Apps) ->
+    StartFun = fun() -> [application:start(App) || App <- Apps] end,
     rpc:call(Node, erlang, apply, [StartFun, []]).
 
+collect_deps(_Node, [], Collected) ->
+    Deps = lists:foldl(fun(App, Acc) ->
+                               case lists:member(App, Acc) of
+                                   true -> Acc;
+                                   false -> [App|Acc]
+                               end end, [], lists:reverse(Collected)),
+    lists:reverse(Deps);
+collect_deps(Node, [App|Applications], Collected) ->
+    rpc:call(Node, application, load, [App]),
+    {ok, Deps} = rpc:call(Node, application, get_key, [App, applications]),
+    collect_deps(Node, Applications ++ Deps, Collected ++ Deps).
 
 %% AppDir is the location to clone to
 %% Url is the URL of the hg repo
